48c74ac931199938e6ff989b192751004ab372b4
Ryan Tanevski <rtan4242@uni.sydney.edu.au>
2023-09-05 21:40:55 +1000

    set up fields, and id's of buttons, also needed to pull Dim's file

--- src/main/java/com/example/demo/Checkout.java
+++ src/main/java/com/example/demo/Checkout.java
@@ -1,4 +1,43 @@
 package com.example.demo;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 public class Checkout {
+
+    @FXML
+    private TextField Address;
+
+    @FXML
+    private Button ChangeItems;
+
+    @FXML
+    private Button Payment;
+
+    @FXML
+    private TextField Phone;
+
+    @FXML
+    private TextField PostCode;
+
+    @FXML
+    private TableColumn<?, ?> Price;
+
+    @FXML
+    private TableColumn<?, ?> Quantity;
+
+    @FXML
+    private TextField State;
+
+    @FXML
+    private TableColumn<?, ?> item;
+
+    @FXML
+    void toMenu(ActionEvent event) {
+
+        }
 }
