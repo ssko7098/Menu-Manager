abaa3362041a60db4a4cdc972cbcfe28db7d10b6
Ryan Tanevski <rtan4242@uni.sydney.edu.au>
2023-09-11 21:16:59 +1000

    added paymeny and delivery options

--- src/main/java/com/example/demo/Checkout.java
+++ src/main/java/com/example/demo/Checkout.java
@@ -1,19 +1,35 @@
 package com.example.demo;
 import javafx.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 
-public class Checkout {
+public class Checkout implements Initializable {
 
     @FXML
     private TextField Address;
 
     @FXML
+    private TextField CVC;
+
+    @FXML
+    private TextField Card;
+
+    @FXML
     private Button ChangeItems;
+
+    @FXML
+    private TextField Expiry;
+
+    @FXML
+    private TextField Name;
 
     @FXML
     private Button Payment;
@@ -37,7 +53,18 @@ public class Checkout {
     private TableColumn<?, ?> item;
 
     @FXML
-    void toMenu(ActionEvent event) {
+    void toMenu(ActionEvent event) throws IOException {
+        HelloApplication changeOrder = new HelloApplication();
+        changeOrder.changeScene("initialMenu.fxml");
+    }
 
-        }
+    @FXML
+    void toCompletion(ActionEvent event) throws IOException {
+        HelloApplication toEnd = new HelloApplication();
+        toEnd.changeScene("completion.fxml");
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
