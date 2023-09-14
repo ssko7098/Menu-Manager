81113338650c48d7b888115fd475856ec688ab11
Seb <ssko7098@uni.sydney.edu.au>
2023-09-14 17:04:38 +1000

    resolved conflict

--- src/test/java/com/example/demo/LoginTesterFile.java
+++ src/test/java/com/example/demo/LoginTesterFile.java
@@ -1,39 +1,22 @@
 package com.example.demo;
 
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import org.json.simple.parser.ParseException;
-import org.junit.Assert;
-import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxRobot;
-import org.testfx.api.FxToolkit;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.Start;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.util.NodeQueryUtils.hasText;
 
 public class LoginTesterFile {
 
-    public LoginTest test;
+    public static LoginTest test;
 
-    @BeforeEach
-    public void setup() throws FileNotFoundException {
+
+    @BeforeAll
+    public static void setup() {
         test = new LoginTest();
     }
 
@@ -41,22 +24,10 @@ public class LoginTesterFile {
         test.username = new TextField("admin");
         test.password = new PasswordField();
         test.passwordText = new TextField();
-//        test.showPassword = new ImageView();
         test.wrongLogin = new Label();
         test.password.setText("1234");
     }
 
-    @Start
-    public void start(Stage primaryStage) throws IOException {
-        Stage stage = primaryStage;
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
-        stage.resizableProperty();
-        stage.setTitle("Menu Manager");
-        stage.setScene(scene);
-        stage.show();
-
-    }
 
     @Test
     void testingTestFunction() throws IOException, ParseException {
