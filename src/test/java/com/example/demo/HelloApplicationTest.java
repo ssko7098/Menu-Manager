de24595175f1dec5ad282312cd07f5881a8091d2
Ethan Acevski <eace4343@uni.sydney.edu.au>
2023-09-14 11:12:51 +1000

    JUnit JavaFx Testing - HelloApplication 88%, Overall 4% Test Coverage

--- src/test/java/com/example/demo/HelloApplicationTest.java
+++ src/test/java/com/example/demo/HelloApplicationTest.java
@@ -5,16 +5,45 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(ApplicationExtension.class)
 public class HelloApplicationTest extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+
+    public Stage stage;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(HelloApplication.class);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        FxToolkit.cleanupStages();
+        FxToolkit.cleanupApplication(HelloApplication.class.newInstance());
+    }
+
+    @Start
+    public void start(Stage primaryStage) throws IOException {
+        Stage stage = primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        stage.resizableProperty();
+        stage.setTitle("Menu Manager");
+        stage.setScene(scene);
+        stage.show();
     }
 
     private volatile boolean success = false;
@@ -48,6 +77,25 @@ public class HelloApplicationTest extends Application {
         assertTrue(success);
 
     }
-    HelloApplication app = new HelloApplication();
+
+
+    @Test
+    public void sceneChangeTest() throws IOException, TimeoutException {
+
+        //Creating new HelloApplication Object
+        HelloApplication app = new HelloApplication();
+
+        //The String of the fxml file for the next stage to go to
+        String nextStage = "SignUP.fxml";
+
+        //Setting old stage to the current stage used to initialise the application - "hello-view.fxml"
+        Stage oldStage = FxToolkit.registerPrimaryStage();
+
+        //Call the scene change using the String of the FXML file for the next stage
+        app.changeScene(nextStage);
+
+        //Test that the stages are different to ensure that the scene change has occured
+        assertNotEquals(oldStage,stage);
+    }
 
 }
