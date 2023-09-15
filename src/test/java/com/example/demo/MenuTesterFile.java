62ccfee9217579b400e39e3ffda3172b92c933f4
Alex Photios <apho9526@uni.sydney.edu.au>
2023-09-15 12:15:49 +1000

    Quanitiy testing for item

--- src/test/java/com/example/demo/MenuTesterFile.java
+++ src/test/java/com/example/demo/MenuTesterFile.java
@@ -103,4 +103,13 @@ public class MenuTesterFile {
 //        o.searchOrder();
     }
 
+    @Test
+    void itemQuantityTest() throws IOException, ParseException {
+        Item item1 = new Item("bob", "bob is cool", 4.8, 7);
+        item1.setQuantity(8);
+        int a = item1.getQuantity();
+        assertEquals(a,8);
+    }
+
+
 }
