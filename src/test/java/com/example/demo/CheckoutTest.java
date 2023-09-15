e371a8add9c2f2f11428343f98b1b0cada56104c
Alex Photios <apho9526@uni.sydney.edu.au>
2023-09-15 17:32:51 +1000

    added Checkout Test File

---
+++ src/test/java/com/example/demo/CheckoutTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CheckoutTest{
+
+}
