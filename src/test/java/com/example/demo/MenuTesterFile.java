28e864107d031dc886efd66e3815003d9a01aea3
Alex Photios <apho9526@uni.sydney.edu.au>
2023-09-16 11:11:40 +1000

    import adjust JavaFX Remove MenuTester

--- src/test/java/com/example/demo/MenuTesterFile.java
+++ src/test/java/com/example/demo/MenuTesterFile.java
@@ -1,32 +1,45 @@
+//package com.example.demo;
+//
+//import javafx.application.Platform;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
+//import javafx.stage.Stage;
+//import org.json.simple.parser.ParseException;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.testfx.api.FxRobot;
+//import org.testfx.framework.junit5.ApplicationExtension;
+//import org.testfx.framework.junit5.Start;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.net.URL;
+//import java.util.Enumeration;
+//import java.util.ResourceBundle;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.testfx.api.FxAssert.verifyThat;
+//import static org.testfx.util.NodeQueryUtils.hasText;
+
 package com.example.demo;
 
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.application.Application;
 import javafx.stage.Stage;
-import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.testfx.api.FxRobot;
-import org.testfx.framework.junit5.ApplicationExtension;
-import org.testfx.framework.junit5.Start;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.ResourceBundle;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.util.NodeQueryUtils.hasText;
+import static org.junit.Assert.*;
+import org.json.simple.parser.ParseException;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(ApplicationExtension.class)
+//@ExtendWith(ApplicationExtension.class)
 public class MenuTesterFile {
 //    @Start
 //    public void start(Stage primaryStage) throws IOException {
