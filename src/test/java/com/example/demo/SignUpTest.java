c3b4a754948796514b7e9d0e911203d11c33e8ea
Ethan Acevski <eace4343@uni.sydney.edu.au>
2023-09-16 10:42:48 +1000

    JUnit JavaFx Testing - SignUp Testing File Initialised

---
+++ src/test/java/com/example/demo/SignUpTest.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.hasText;
+
+@ExtendWith(ApplicationExtension.class)
+public class SignUpTest {
+    @Start
+    public void start(Stage primaryStage) throws IOException {
+        Stage stage = primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("SignUp.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        stage.resizableProperty();
+        stage.setTitle("Menu Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
